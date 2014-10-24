d38852ee1dfebe70053734efc63aef048d1f7be3
Jorge Enrique Chavez Palma <jorge_clan8@hotmail.com>
2014-10-24 05:09:11 -0500

    Button Add quote

--- index.php
+++ index.php
@@ -13,6 +13,14 @@ extract($phrase);
 	<title>Sublime Text Quotes</title>
 	<link href='http://fonts.googleapis.com/css?family=Chau+Philomene+One' rel='stylesheet' type='text/css'>
 	<link rel="stylesheet" href="css/styles.css">
+	<div id="fb-root"></div>
+	<script>(function(d, s, id) {
+	  var js, fjs = d.getElementsByTagName(s)[0];
+	  if (d.getElementById(id)) return;
+	  js = d.createElement(s); js.id = id;
+	  js.src = "//connect.facebook.net/es_LA/sdk.js#xfbml=1&appId=131923847009565&version=v2.0";
+	  fjs.parentNode.insertBefore(js, fjs);
+	}(document, 'script', 'facebook-jssdk'));</script>
 </head>
 <body>
 	<div id="window">
@@ -24,7 +32,7 @@ extract($phrase);
 		<div id="space">
 			<div id="lines">
 				<?php 
-				for($i = 1; $i < 15;$i++){
+				for($i = 1; $i < 7;$i++){
 					echo "<span>".$i."</span>";
 					} 
 				?>
@@ -40,9 +48,11 @@ extract($phrase);
 				<div id="img-author" style="background-image: url('<?= $img_src; ?>');">
 				</div>
 				<?php } ?>
+				<div class="fb-like" data-href="https://developers.facebook.com/docs/plugins/" data-layout="button" data-action="like" data-show-faces="false" data-share="true"></div>
 			</div>
 		</div>
 	</div>
+	<a href="#" title="Add new quote" id="new-quote">+</a>
 	<script src="js/jquery.js" type="text/javascript" charset="utf-8"></script>
 	<script src="js/script.js" type="text/javascript" charset="utf-8" async defer></script>
 </body>
